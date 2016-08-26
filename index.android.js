debeffe2be8f5eb60e940a84b830a3d395596df8
zhouyu <zhouyu@videbo.com>
2016-08-26 17:09:03 +0800

    first commit

---
+++ index.android.js
@@ -0,0 +1,61 @@
+'use strict';
+
+import React from 'react';
+import {
+  AppRegistry,
+  Image,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+var MOCKED_MOVIES_DATA = [
+  {title: 'Title', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
+];
+
+var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
+
+class VideboDiscovery  extends  React.Component {
+  render() {
+   var movie = MOCKED_MOVIES_DATA[0];
+   return (
+     <View style={styles.container}>
+       <Image
+         source={{uri: movie.posters.thumbnail}}
+         style={styles.thumbnail}
+        />
+       <View style={styles.rightContainer}>
+         <Text style={styles.title}>{movie.title}</Text>
+         <Text style={styles.year}>{movie.year}</Text>
+       </View>
+     </View>
+   );
+  }
+}
+
+var styles = StyleSheet.create({
+   container: {
+       flex: 1,
+       flexDirection: 'row',
+       justifyContent: 'center',
+       alignItems: 'center',
+       backgroundColor: '#F5FCFF',
+   },
+   thumbnail: {
+     width: 53,
+     height: 81,
+   },
+   rightContainer: {
+    flex: 1,
+   },
+   title: {
+   fontSize: 20,
+   marginBottom: 8,
+   textAlign: 'center',
+   },
+   year: {
+      textAlign: 'center',
+   },
+});
+
+AppRegistry.registerComponent('VideboDiscovery', () => VideboDiscovery);
